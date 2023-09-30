cecf7ff27e2ce432b0c21a631783c10be1adccee
cochae <huisu121@gmail.com>
2023-09-30 18:50:46 +0900

    refactor: 함수 추출하기

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,27 +1,32 @@
 export function printOwing(invoice) {
-  let outstanding = 0;
+  printBanner();
 
+  const outstanding = calculateOutstanding(invoice);
+
+  const dueDate = calculateDueDate();
+
+  printDetails({ invoice, outstanding, dueDate });
+}
+
+function printBanner() {
   console.log('***********************');
   console.log('**** Customer Owes ****');
   console.log('***********************');
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
 
-  // record due date
+function calculateDueDate() {
   const today = new Date();
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
 
-  //print details
+function printDetails({ invoice, outstanding, dueDate }) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  console.log(`due: ${dueDate.toLocaleDateString()}`);
 }
 
 const invoice = {
