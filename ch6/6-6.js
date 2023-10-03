467acfc704d0d9724895b6918b37aacfbddeb99d
cochae <huisu121@gmail.com>
2023-10-03 15:27:29 +0900

    refactor: 변수 캡슐화하기 - class 선언

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,22 @@
-const defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+  #firstName;
+  #lastName;
+  constructor(data) {
+    this.#firstName = data.firstName;
+    this.#lastName = data.lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+}
+
+const defaultOwner = new Person({ firstName: '마틴', lastName: '파울러' });
 
 export function getDefaultOwner() {
-  return { ...defaultOwner };
+  return defaultOwner;
 }
