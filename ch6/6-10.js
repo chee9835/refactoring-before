06ce6ab7781988457b3d38383119bf2928bd3083
cochae <huisu121@gmail.com>
2023-10-03 16:18:55 +0900

    refactor: 여러 함수를 변환 함수로 묶기

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,7 +1,23 @@
+import _ from 'lodash';
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+
+export function enrichReading(original) {
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(0, result.baseCharge - taxThreshold());
+  return result;
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
+}
+
+function taxThreshold() {
+  return 0.1;
 }
 
 export function baseRate(month, year) {
