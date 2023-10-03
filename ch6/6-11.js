cc995e7457a22cfe3159741ffb7a1f6c473a3b57
cochae <huisu121@gmail.com>
2023-10-03 17:17:28 +0900

    refactor: 단계 쪼개기

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,16 +1,21 @@
 export function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
+  const discount = calculateDiscountedPrice(product, quantity);
+  const shippingCost =
+    quantity * calculateShippingPerCase(basePrice, shippingMethod);
+  return basePrice - discount + shippingCost;
+}
+
+function calculateDiscountedPrice(product, quantity) {
+  Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+}
+
+function calculateShippingPerCase(basePrice, shippingMethod) {
+  return basePrice > shippingMethod.discountThreshold
+    ? shippingMethod.discountedFee
+    : shippingMethod.feePerCase;
 }
 
 // 사용 예:
