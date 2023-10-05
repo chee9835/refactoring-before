dd9205ae5c19848e905faa5194ba3df08d72d597
cochae <huisu121@gmail.com>
2023-10-05 22:04:54 +0900

    refactor: 레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,4 +1,32 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  constructor(data) {
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  set name(value) {
+    this.#name = value;
+  }
+  get country() {
+    return this.#country;
+  }
+  set country(value) {
+    this.#country = value;
+  }
+  get rawDate() {
+    return { name: this.#name, country: this.#country };
+  }
+}
+
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+});
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
