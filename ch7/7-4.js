957ce5501727d230accc5b243f5e6a79c266218d
cochae <huisu121@gmail.com>
2023-10-09 12:02:08 +0900

    refactor: 임수 변수를 질의 함수로 바꾸기

--- ch7/7-4.js
+++ ch7/7-4.js
@@ -6,10 +6,16 @@ class Order {
     this.#item = item;
   }
 
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98;
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    const discountFactor = this.basePrice > 1000 ? 0.95 : 0.98;
+    return this.basePrice * discountFactor;
   }
 }
