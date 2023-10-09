77f73dd3c18bc4d9836aa5354bbb7d191a8f59ba
cochae <huisu121@gmail.com>
2023-10-09 15:26:25 +0900

    refactor: 반복문 쪼개기

--- ch8/8-7.js
+++ ch8/8-7.js
@@ -1,10 +1,8 @@
 export function reportYoungestAgeAndTotalSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
-  }
-
+  const youngest = Math.min(...people.map(p => p.age));
+  const totalSalary = people.reduce(
+    (total, person) => (total += person.salary),
+    0
+  );
   return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
 }
