ba45ed91b0fe311eb6d86e514abbf98fcaf645d5
cochae <huisu121@gmail.com>
2023-10-09 11:48:28 +0900

    refactor: 기본형을 객체로 바꾸기 - 메서드에서 사용할 우선순위 클래스 추가

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,17 +1,44 @@
 export class Order {
   constructor(data) {
-    this.priority = data.priority;
+    this.priority = data;
   }
 
   isHighPriority() {
-    return 'high' === this.priority || 'rush' === this.priority;
+    return this.priority.higherThan(new Priority('normal'));
+  }
+}
+
+class Priority {
+  #value;
+  constructor(value) {
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value;
+    } else {
+      throw new Error(`${value} is invalid for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().indexOf(this.#value);
+  }
+
+  equals(other) {
+    return this.index === other.index;
+  }
+
+  higherThan(other) {
+    return this.index > other.index;
+  }
+
+  static legalValues() {
+    return ['low', 'normal', 'high', 'rush'];
   }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order(new Priority('normal')),
+  new Order(new Priority('high')),
+  new Order(new Priority('rush')),
 ];
 
 const highPriorityCount = orders.filter(o => o.isHighPriority()).length;
