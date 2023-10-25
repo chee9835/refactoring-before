8af2d0c3c27992bed4d31e601aae6fe0f9dd0b88
cochae <huisu121@gmail.com>
2023-10-25 11:32:49 +0900

    refactor: 값을 참조로 바꾸기

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,7 +1,13 @@
+const customerRepository = new CustomerRepository();
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId, data.customerName)
+);
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
@@ -10,11 +16,41 @@ class Order {
 }
 
 class Customer {
-  constructor(id) {
+  constructor(id, name) {
     this._id = id;
+    this._name = name;
   }
 
   get id() {
     return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    this._name = value;
+  }
+}
+
+class CustomerRepository {
+  #customers;
+
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id, name) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id, name));
+    }
+    return this.findCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
+  }
+
+  changeCustomerName(name) {
+    this.#customers.name(name);
   }
 }
