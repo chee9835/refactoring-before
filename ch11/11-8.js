28c356c851f78eafcc598664a76ff550129b1157
cochae <huisu121@gmail.com>
2023-11-07 14:45:11 +0900

    refactor: 생성자를 팩터리 함수로 바꾸기

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,4 +1,5 @@
 export class Employee {
+  // 자바스크립트의 생성자는 private 필드로 선언 불가
   constructor(name, typeCode) {
     this._name = name;
     this._typeCode = typeCode;
@@ -14,4 +15,18 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+
+  static createEngineer(name) {
+    return new Employee(name, 'E');
+  }
+
+  static createManager(name) {
+    return new Employee(name, 'M');
+  }
+
+  static createSalesman(name) {
+    return new Employee(name, 'S');
+  }
 }
+
+const employee = Employee.createEngineer('coco');
